5ea913f7e89c5e7964363d6cacc1abcb4d1936f6
LudovicLeGuen <>
2023-06-28 19:15:30 +0000

    added stripes and elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NO3ChF76CufBdvKkJTW1jz01g98EPHMQ0kAGAwc33wdUd00p6ENtleKvlJeMcAojKGLwew18TSnoFPt6czl6Ggn00T2kI76nF',
+        'client_secret': 'sk_test_51NO3ChF76CufBdvKQHXuWL3mv85L4a4eh7MIM1ahVHQvS4t889mfmTHAozF1GIIeJ9NcodOdi6Z8lK1zUIyRskSH00SGItf5Mv',
     }
 
     return render(request, template, context)
